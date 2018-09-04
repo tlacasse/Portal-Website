c64b6bfcaa19c09dffba29244677f9612c49db85
Thomas Lacasse <thomas@kukt.us>
2018-09-04 14:27:02 -0400

    Build: Add Build Number

--- PortalWebsite/build.ps1
+++ PortalWebsite/build.ps1
@@ -49,67 +49,7 @@ Write-Host '-----------------------------' -ForegroundColor Green
 
 & (Resolve-Path 'C:\Users\**\AppData\Roaming\npm\gulp.cmd') --env $env
 
+$number = [int]([Environment]::GetEnvironmentVariable('portal_build_number', 'User')) + 1
+[Environment]::SetEnvironmentVariable('portal_build_number', "$number", 'User')
 
-
-<#$iconPath = Join-Path $buildPath 'favicon.ico'
-if (Path-NotExists $iconPath){
-	Copy-Item (Join-Path $PSScriptRoot 'favicon.ico') $iconPath -Verbose
-}#>
-
-#& (Resolve-Path 'C:\Users\**\AppData\Roaming\npm\gulp.cmd') --env $env
-
-<#
-$base = 'C:\Users\XYZ\Documents\GitHub\Portal\Portal'
-$path = $base + '\_Build'
-if (-not (Test-Path ($path))){
-	New-Item -Path $path -ItemType directory
-}
-if (-not (Test-Path ($path + '\Scripts'))){
-	New-Item -Path ($path + '\Scripts') -ItemType directory
-}
-if (-not (Test-Path ($path + '\Portal'))){
-	New-Item -Path ($path + '\Portal') -ItemType directory
-}
-if (-not (Test-Path ($path + '\favicon.ico'))){
-	Copy-Item ($base + '\favicon.ico') $path
-}
-
-Write-Host '-----------------------------' -ForegroundColor Green
-Write-Host 'Removing old files.' -ForegroundColor Green
-Write-Host '-----------------------------' -ForegroundColor Green
-$files = Get-ChildItem -Path $path -Recurse -exclude favicon.ico
-$fileToDelete = @()
-foreach($file in $files){
-	$ignore = $false
-	$ignore = $ignore -or ($file.FullName -like ($path + '\Portal*'))
-	$ignore = $ignore -or ($file.FullName -like ($path + '\Scripts'))
-	$ignore = $ignore -or ($file.FullName -like ($path + '\bin*'))
-	$ignore = $ignore -or ($file.FullName -like ($path + '*.config'))
-	$ignore = $ignore -or ($file.FullName -like ($path + '*.asax*'))
-	if (-not $ignore){
-		$fileToDelete += $file
-	}
-}
-$fileToDelete | Select -ExpandProperty FullName | sort length -Descending | Remove-Item -Verbose
-
-Write-Host '-----------------------------' -ForegroundColor Green
-Write-Host 'Building CSproject.' -ForegroundColor Green
-Write-Host '-----------------------------' -ForegroundColor Green
-$project = $base + '\Portal.csproj'
-$msBuildExe = 'C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe'
-& "$($msBuildExe)" "$($project)" /t:Build /m
-
-Write-Host '-----------------------------' -ForegroundColor Green
-Write-Host 'Gulp build.' -ForegroundColor Green
-Write-Host '-----------------------------' -ForegroundColor Green
-& "C:\Users\XYZ\AppData\Roaming\npm\gulp.cmd" --env $env
-
-Write-Host '-----------------------------' -ForegroundColor Green
-Write-Host '===FINISHED BUILD===' -ForegroundColor Green
-
-$readVersion = [IO.File]::ReadAllText($base + '\_Build\Portal\version.txt')
-$split = $readVersion.split('.')
-$writeVersion = $split[0] + '.' + (([int]$split[1])+1)
-$writeVersion | Out-File ($base + '\_Build\Portal\version.txt')
-Write-Host ('= ' + $writeVersion) -ForegroundColor Green
-#>
+Write-Host ("Build Number: $number") -ForegroundColor 'magenta'
