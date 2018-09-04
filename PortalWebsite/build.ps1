68f7ba7a019a8d239bb22a903c5f2367ad217f24
Thomas Lacasse <thomas@kukt.us>
2018-09-04 14:21:21 -0400

    Add Projects & Build Structure

---
+++ PortalWebsite/build.ps1
@@ -0,0 +1,115 @@
+﻿param(
+	  [string]$env = 'dev'
+	, [string]$buildPath = (Join-Path $PSScriptRoot '_Build')
+)
+
+function Path-NotExists(){
+	param(
+		[string]$pathToTest
+	)
+	return -not (Test-Path($pathToTest))
+} 
+
+Write-Host '-----------------------------' -ForegroundColor Green
+Write-Host 'Checking File Existence.' -ForegroundColor Green
+Write-Host '-----------------------------' -ForegroundColor Green
+
+if (Path-NotExists $buildPath){
+	New-Item -Path $buildPath -ItemType directory -Verbose
+}
+$scriptsPath = Join-Path $buildPath 'Scripts'
+if (Path-NotExists $scriptsPath){
+	New-Item -Path $scriptsPath -ItemType directory -Verbose
+}
+$portalPath = Join-Path $buildPath 'Portal'
+if (Path-NotExists $portalPath){
+	New-Item -Path $portalPath -ItemType directory -Verbose
+}
+robocopy $PSScriptRoot $buildPath 'favicon.ico' 'Web.config' 'index.html' /COPY:DATS
+
+Write-Host '-----------------------------' -ForegroundColor Green
+Write-Host 'Building CSproject.' -ForegroundColor Green
+Write-Host '-----------------------------' -ForegroundColor Green
+
+$project = Join-Path $PSScriptRoot 'PortalWebsite.csproj'
+$msBuildExe = Resolve-Path 'C:\Program Files (x86)\MSBuild\**\Bin\msbuild.exe'
+& $msBuildExe $project /t:Build /m
+
+Write-Host '-----------------------------' -ForegroundColor Green
+Write-Host 'Copy Binaries.' -ForegroundColor Green
+Write-Host '-----------------------------' -ForegroundColor Green
+
+$sourceBin = Join-Path $PSScriptRoot 'bin'
+$destBin = Join-Path $buildPath 'bin'
+robocopy $sourceBin $destBin /COPY:DATS
+
+Write-Host '-----------------------------' -ForegroundColor Green
+Write-Host 'Gulp build.' -ForegroundColor Green
+Write-Host '-----------------------------' -ForegroundColor Green
+
+& (Resolve-Path 'C:\Users\**\AppData\Roaming\npm\gulp.cmd') --env $env
+
+
+
+<#$iconPath = Join-Path $buildPath 'favicon.ico'
+if (Path-NotExists $iconPath){
+	Copy-Item (Join-Path $PSScriptRoot 'favicon.ico') $iconPath -Verbose
+}#>
+
+#& (Resolve-Path 'C:\Users\**\AppData\Roaming\npm\gulp.cmd') --env $env
+
+<#
+$base = 'C:\Users\XYZ\Documents\GitHub\Portal\Portal'
+$path = $base + '\_Build'
+if (-not (Test-Path ($path))){
+	New-Item -Path $path -ItemType directory
+}
+if (-not (Test-Path ($path + '\Scripts'))){
+	New-Item -Path ($path + '\Scripts') -ItemType directory
+}
+if (-not (Test-Path ($path + '\Portal'))){
+	New-Item -Path ($path + '\Portal') -ItemType directory
+}
+if (-not (Test-Path ($path + '\favicon.ico'))){
+	Copy-Item ($base + '\favicon.ico') $path
+}
+
+Write-Host '-----------------------------' -ForegroundColor Green
+Write-Host 'Removing old files.' -ForegroundColor Green
+Write-Host '-----------------------------' -ForegroundColor Green
+$files = Get-ChildItem -Path $path -Recurse -exclude favicon.ico
+$fileToDelete = @()
+foreach($file in $files){
+	$ignore = $false
+	$ignore = $ignore -or ($file.FullName -like ($path + '\Portal*'))
+	$ignore = $ignore -or ($file.FullName -like ($path + '\Scripts'))
+	$ignore = $ignore -or ($file.FullName -like ($path + '\bin*'))
+	$ignore = $ignore -or ($file.FullName -like ($path + '*.config'))
+	$ignore = $ignore -or ($file.FullName -like ($path + '*.asax*'))
+	if (-not $ignore){
+		$fileToDelete += $file
+	}
+}
+$fileToDelete | Select -ExpandProperty FullName | sort length -Descending | Remove-Item -Verbose
+
+Write-Host '-----------------------------' -ForegroundColor Green
+Write-Host 'Building CSproject.' -ForegroundColor Green
+Write-Host '-----------------------------' -ForegroundColor Green
+$project = $base + '\Portal.csproj'
+$msBuildExe = 'C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe'
+& "$($msBuildExe)" "$($project)" /t:Build /m
+
+Write-Host '-----------------------------' -ForegroundColor Green
+Write-Host 'Gulp build.' -ForegroundColor Green
+Write-Host '-----------------------------' -ForegroundColor Green
+& "C:\Users\XYZ\AppData\Roaming\npm\gulp.cmd" --env $env
+
+Write-Host '-----------------------------' -ForegroundColor Green
+Write-Host '===FINISHED BUILD===' -ForegroundColor Green
+
+$readVersion = [IO.File]::ReadAllText($base + '\_Build\Portal\version.txt')
+$split = $readVersion.split('.')
+$writeVersion = $split[0] + '.' + (([int]$split[1])+1)
+$writeVersion | Out-File ($base + '\_Build\Portal\version.txt')
+Write-Host ('= ' + $writeVersion) -ForegroundColor Green
+#>
---
+++ PortalWebsite/gulpfile.js
@@ -0,0 +1,36 @@
+﻿var gulp = require('gulp');
+
+var cleanCSS = require("gulp-clean-css");
+var concat = require("gulp-concat");
+var inject = require("gulp-inject");
+var minify = require("gulp-minify");
+var noop = require("gulp-noop");
+var rename = require('gulp-rename');
+var sass = require("gulp-sass");
+var minimist = require('minimist');
+
+var knownOptions = {
+    string: 'env',
+    default: {
+        env: 'dev'
+    }
+};
+
+var options = minimist(process.argv.slice(2), knownOptions);
+
+function isProduction() {
+    return options.env.toUpperCase() === 'release'.toUpperCase();
+}
+
+var d = new Date();
+var dateAppend = "_" + String(d.getFullYear()) + "-" + String(d.getMonth() + 1) + "-" + String(d.getDate())
+    + "_" + String(d.getHours()) + "-" + String(d.getMinutes()) + "-" + String(d.getSeconds());
+
+var build = '_Build';
+
+///////////////////////////////////////////////////////////////
+// Tasks
+
+//gulp.task('default', gulp.series('index', 'views', 'css', 'app', 'angular', 'inject', 'bin', 'asp'));
+
+gulp.task('default', function () { });
