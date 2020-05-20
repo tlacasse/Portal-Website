d42fcc092ec3c14571d65b2119d9a2b99fef4eb4
Thomas Lacasse <thomas@kukt.us>
2020-05-20 14:06:46 -0400

    project rework: delete old files

--- Portal/sqlite/PortalIcon.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE PortalIcon (
-    Id          INTEGER       PRIMARY KEY ON CONFLICT FAIL AUTOINCREMENT,
-    Name        VARCHAR (30)  UNIQUE ON CONFLICT FAIL,
-    Image       VARCHAR (10),
-    Link        VARCHAR (500),
-    DateCreated DATETIME,
-    DateChanged DATETIME
-);
